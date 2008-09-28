efd801ea6c137eb3505e68fae466110d550cfa2c
KN Webcie <webcie@karpenoktem.nl>
2008-09-28 12:25:30 +0000

    Changes to admin and model, remove import-ledenlijst
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- kn/leden/admin.py
+++ kn/leden/admin.py
@@ -1,9 +1,37 @@
-from models import Study, Member, EduInstitute, Commission
+from django.shortcuts import render_to_response, get_object_or_404
+from models import Study, Member, EduInstitute, Commission, Seat, Alias
+from django.template import RequestContext
+from django.contrib.auth.admin import AdminPasswordChangeForm, UserAdmin
+from django.utils.html import escape
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext, ugettext_lazy as _
 
+class MemberAdmin(admin.ModelAdmin):
+	change_password_form = AdminPasswordChangeForm
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+	list_filter = ('is_staff', 'is_superuser')
+	search_fields = ('username', 'first_name', 'last_name', 'email')
+	ordering = ('username', )
+	filter_horizontal = ('user_permissions',)
+	
+	def __call__(self, request, url):
+		if url is None:
+			return self.changelist_view(request)
+		if url.endswith('password'):
+			return HttpResponseRedirect('../../../../auth/user/%s/password' %
+					url.split('/')[0])
+		return super(MemberAdmin, self).__call__(request, url)
+
+class CommissionAdmin(admin.ModelAdmin):
+	search_fields = ('name', 'description')
+	ordering = ('name', )
+	filter_horizontal = ('permissions', )
+	list_display = ('name', 'humanName')
 
 admin.site.register(Study)
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(EduInstitute)
-admin.site.register(Commission)
-
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Seat)
+admin.site.register(Alias)
--- kn/leden/models.py
+++ kn/leden/models.py
@@ -16,30 +16,39 @@ class Study(models.Model):
 	def __unicode__(self):
 		return self.name
 
-class Member(models.Model):
-	user = models.OneToOneField(User)
-	dateOfBirth = models.DateField(null=True)
-	dateJoined = models.DateField(null=True)
-
+class Member(User):
+	dateOfBirth = models.DateField(null=True, blank=True)
+	dateJoined = models.DateField(null=True, blank=True)
+	
 	addr_street = models.CharField(max_length=100)
 	addr_number = models.CharField(max_length=20)
 	addr_zipCode = models.CharField(max_length=10)
 	addr_city = models.CharField(max_length=80)
 	
-	telephone = models.IntegerField(null=True)
-	studentNumber = models.IntegerField(unique=True, null=True)
+	gender = models.CharField(max_length=1, blank=True)
+	telephone = models.CharField(max_length=20, null=True)
+	studentNumber = models.CharField(max_length=20, unique=True, null=True, blank=True)
 	institute = models.ForeignKey('EduInstitute', null=True)
 	study = models.ForeignKey('Study', null=True)
-	commissions = models.ManyToManyField('Commission')
 
-	def __unicode__(self):
-		return unicode(self.user)
-
-class Commission(models.Model):
-	group = models.OneToOneField(Group)
-	name = models.CharField(max_length=120)
+class Commission(Group):
+	humanName = models.CharField(max_length=120)
 	decription = models.TextField()
-	hoofd = models.ForeignKey('Member')
-	
+
+class Seat(models.Model):
+	name = models.CharField(max_length=80)
+	humanName = models.CharField(max_length=120)
+	description = models.TextField()
+	commission = models.ForeignKey('Commission')
+	member = models.ForeignKey('Member')
+	isGlobal = models.BooleanField()
+
 	def __unicode__(self):
-		return unicode(self.name)
+		return unicode(self.humanName) + " (" + unicode(self.commission) + ")"
+
+class Alias(models.Model):
+	source = models.CharField(max_length=80)
+	target = models.CharField(max_length=80)
+
+	def __unicode__(self):
+		return unicode(self.source + " -> " + self.target)
