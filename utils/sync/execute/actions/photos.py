c98f845595d2ba36441e458014ec16424362a19b
KN Webcie <webcie@karpenoktem.nl>
2008-10-19 19:40:11 +0000

    Adds execution of symlink and mkdir commands of the photos action. Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ utils/sync/execute/actions/photos.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+
+import os
+
+def execute(args):
+	if len(args) < 1:
+		print "Missing action"
+		return -1
+	cmd = args[0]
+	if cmd == 'mkdir':
+		if len(args)!=2:
+			print "Not exactly one argument given."
+			return -1
+		os.mkdir(args[1])
+	elif cmd == "symlink":
+		if len(args)!=3:
+			print "Not exactly two arguments given."
+			return -1
+		os.symlink(args[1],args[2])
+	else:
+		print "Unknown command: %s" % cmd
+		return -1
+	
+	
+
