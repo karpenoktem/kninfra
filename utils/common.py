7cc586a0d9c4bafb2c1014f2a6c25eaf0f144b4d
KN Webcie <webcie@karpenoktem.nl>
2008-11-01 15:44:53 +0000

    sync: use one single common.py
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ utils/common.py
@@ -0,0 +1,23 @@
+from __future__ import with_statement
+
+def read_ssv_file(filename):
+        """ Reads values seperated by spaces in a simple one line file """
+        with open(filename) as f:
+                return f.readline()[:-1].split(' ')
+
+def sesc(t):
+	return t.replace('\\','\\\\').replace(' ','\\ ')
+	
+
+DOMAIN = 'karpenoktem.nl'
+LISTDOMAIN = 'lists.karpenoktem.nl'
+
+EMAIL_ALLOWED = frozenset(
+		    map(lambda x: chr(ord('a') + x), xrange(26)) +
+		    map(lambda x: chr(ord('A') + x), xrange(26)) +
+		    map(lambda x: chr(ord('0') + x), xrange(10)) +
+		    ['.', '-'])
+
+GALLERY_PATH = '/var/galleries/kn/'
+MEMBERS_HOME = '/home/kn/'
+MEMBER_PHOTO_DIR = 'fotos/'
