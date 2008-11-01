988330cb98e3b4fd3844f4df493d87676f7e5fa8
KN Webcie <webcie@karpenoktem.nl>
2008-11-01 15:23:26 +0000

    shell: Fix cadd. Add member, comm and ml.
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/shell.py
+++ utils/shell.py
@@ -1,4 +1,5 @@
 import _import
+import Mailman.MailList
 from kn.leden.models import Seat, Commission, Member, Alias
 
 #
@@ -7,7 +8,7 @@ from kn.leden.models import Seat, Commission, Member, Alias
 
 def cadd(commission, user):
 	""" Add <user> to <commission> """
-	Commission.objects.get(name=commission).user_set.add(User.objects.get(username=user))
+	Commission.objects.get(name=commission).user_set.add(Member.objects.get(username=user))
 
 def sadd(commission, seat, user):
 	""" Creates the seat <seat> for <commission> with <user> """
@@ -22,3 +23,16 @@ def aadd(source, target):
 	""" Creates the alias <source> -> <target> """
 	a = Alias(source=source, target=target)
 	a.save()
+
+def member(username):
+	""" Gets the <Member> with username <username> """
+	return Member.objects.get(username=username)
+
+def comm(name):
+	""" Gets the <Commission> with name <name> """
+	return Commission.objects.get(name=name)
+
+def ml(name):
+	""" Gets the Mailman.MailList.MailList object for the list
+		<name>. (Unlocked) """
+	return Mailman.MailList.MailList(name, False)
