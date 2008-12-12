7e793526abaa131b902862c7e0eb03e109eae04e
KN Webcie <webcie@karpenoktem.nl>
2008-12-12 16:02:07 +0000

    kn.leden: add template tags for email
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ kn/leden/templatetags/leden.py
@@ -0,0 +1,16 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+
+register = template.Library()
+
+@stringfilter
+@register.filter(name='email')
+def email_filter(value):
+	n, r = conditional_escape(value).split('@',1)
+	d, e = r.rsplit('.',1)
+	return mark_safe(("<script type='text/javascript'>email("+
+		"'%s', '%s', '%s')</script><noscript>X@Y.Z waar Z=%s,"+
+		" Y=%s, X=%s</noscript>") % (\
+		e, d, n, e, d, n))
