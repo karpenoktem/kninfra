c722f582d675b4633d6693c4a68981e3bff065f8
KN Webcie <webcie@karpenoktem.nl>
2009-04-01 15:50:09 +0000

    utils/fools-email: April Fools
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ utils/fools-email.py
@@ -0,0 +1,19 @@
+from __future__ import with_statement
+
+import _import
+from common import *
+from kn.leden.models import KnGroup, KnUser
+
+def forum_email():
+	with open('fools-email.template', 'r') as f:
+		templ = f.read()
+	l5 = KnGroup.objects.get(name=MEMBER_GROUP).user_set.all()
+	for m in l5:
+		txt = templ % ({'username': m.username})
+		m.email_user(
+			'LEES DIT: BELANGRIJK',
+			txt, from_email='karpenoktemwebcie@hotmail.com')
+		print m.username
+
+if __name__ == '__main__':
+	forum_email()
