75a84c55728c0f722fef88046f4a1b525ab8a266
KN Webcie <webcie@karpenoktem.nl>
2009-04-14 17:06:52 +0000

    utils/detailedList: support for multiple groups
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/detailedList.py
+++ utils/detailedList.py
@@ -4,18 +4,23 @@ import sys
 from common import *
 from kn.leden.models import KnUser, KnGroup
 
-g = KnGroup.objects.get(name=MEMBER_GROUP if len(sys.argv) == 1
-					  else sys.argv[1])
+gs = map(lambda x: KnGroup.objects.get(name=x),
+	 [MEMBER_GROUP] if len(sys.argv) == 1 else sys.argv[1:])
+had = set()
 
-for _u in g.user_set.all():
-	u = _u.knuser
-	print "%-13s %-17s %-1s %-10s %-25s %-26s %-8s %-10s %s" % \
-				    (u.first_name,
-				     u.last_name,
-				     u.gender,
-				     u.dateOfBirth,
-				     u.primary_email,
-				     u.addr_street + ' ' + u.addr_number,
-				     u.addr_zipCode,
-				     u.addr_city,
-				     u.telephone)
+for g in gs:
+	for _u in g.user_set.all():
+		if _u.pk in had:
+			continue
+		had.add(_u.pk)
+		u = _u.knuser
+		print "%-13s %-17s %-1s %-10s %-25s %-26s %-8s %-10s %s" % \
+					    (u.first_name,
+					     u.last_name,
+					     u.gender,
+					     u.dateOfBirth,
+					     u.primary_email,
+					     u.addr_street + ' ' + u.addr_number,
+					     u.addr_zipCode,
+					     u.addr_city,
+					     u.telephone)
