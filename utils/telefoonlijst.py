fba4c36c0cff6522ce41928ecedc7db337387ec2
KN Webcie <webcie@karpenoktem.nl>
2009-06-16 17:07:42 +0000

    telefoonlijst: use args_to_users
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/telefoonlijst.py
+++ utils/telefoonlijst.py
@@ -2,9 +2,9 @@ import _import
 
 from common import *
 from kn.leden.models import KnGroup
+import sys
 
-for m in KnGroup.objects.get(name=MEMBER_GROUP).user_set.order_by(
-		'first_name').all():
+for m in args_to_users(sys.argv[1:]):
 	print "%15s%20s%15s" % (m.first_name,
 				m.last_name,
 				m.knuser.telephone)
