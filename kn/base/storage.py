097c32ff63fd789f26c8ebddf470827c444bc6a9
KN Webcie <webcie@karpenoktem.nl>
2009-10-28 00:08:04 +0000

    base: add OurFileSystemStorage which uses STORAGE_* defaults
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ kn/base/storage.py
@@ -0,0 +1,9 @@
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+# Override the defaults on the constructor
+class OurFileSystemStorage(FileSystemStorage):
+	def __init__(self):
+		super(OurFileSystemStorage, self).__init__(
+				settings.STORAGE_ROOT,
+				settings.STORAGE_URL)
