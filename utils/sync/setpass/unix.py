def7c7447b70b45e61b9e9e8348b2f30e16a7145
KN Webcie <webcie@karpenoktem.nl>
2009-11-17 23:34:56 +0000

    utils/sync/setpass/unix: make path to usermod a setting
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/sync/setpass/unix.py
+++ utils/sync/setpass/unix.py
@@ -1,7 +1,8 @@
 from subprocess import call
 import crypt
 from common import pseudo_randstr
+from kn.settings import SYSTEM_USERMOD_PATH
 
 def setpass(user, password):
 	crypthash = crypt.crypt(password, pseudo_randstr(2))
-	call(['/usr/sbin/usermod', '-p', crypthash, user])
+	call([SYSTEM_USERMOD_PATH, '-p', crypthash, user])
