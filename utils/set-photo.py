7dd32ed5f2fa61fd6a2b25a80793619c3d5bb0e0
KN Webcie <webcie@karpenoktem.nl>
2009-11-17 23:21:25 +0000

    utils/set-photo: style fixes
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/set-photo.py
+++ utils/set-photo.py
@@ -1,10 +1,16 @@
 import _import
 
 from common import *
-import sys
 from kn.leden.models import OldKnUser
+import sys
 import subprocess
 
-user = OldKnUser.objects.get(username=sys.argv[1])
+def main():
+	user = OldKnUser.objects.get(username=sys.argv[1])
 
-subprocess.call(['convert', '-resize', '200x', sys.argv[2], 'jpg:/var/django/kn/storage/smoelen/%s.jpg' % user.username])
+	subprocess.call(['convert', '-resize', '200x', sys.argv[2],
+			 'jpg:/var/django/kn/storage/smoelen/%s.jpg' % \
+					 user.username])
+
+if __name__ == '__main__':
+	main()
