2aaf869431749930f513084db8529c6284759be2
Jille Timmermans <jille@karpenoktem.nl>
2010-01-06 12:44:23 +0000

    Added EventSubscriptionAdmin: fixed displaying, filtering, searching and sorting
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- kn/subscriptions/admin.py
+++ kn/subscriptions/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from kn.subscriptions.models import Event, EventSubscription
 
+class EventSubscriptionAdmin(admin.ModelAdmin):
+	list_display = ('user', 'event', 'debit')
+	list_filter = ('event', 'debit', )
+	search_fields = ('user', 'event')
+	ordering = ('id', )
+
 admin.site.register(Event)
-admin.site.register(EventSubscription)
+admin.site.register(EventSubscription, EventSubscriptionAdmin)
