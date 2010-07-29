8a78f5517db1b7ffb49545cef02eef0b3b28ed14
Bas Westerbaan <bas@westerbaan.name>
2010-07-29 13:21:31 +0200

    shell: remove old django.db shell code
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- utils/shell.py
+++ utils/shell.py
@@ -3,46 +3,5 @@ try:
 	import Mailman.MailList
 except ImportError:
 	pass
-from kn.leden.models import (OldSeat, OldKnGroup, OldKnUser, Alias,
-				Seat, KnGroup, KnUser)	
 
-#
-# Very short helper functions for trivial tasks
-#
-
-def cadd(commission, user):
-	""" Add <user> to <commission> """
-	OldKnGroup.objects.get(name=commission).user_set.add(OldKnUser.objects.get(username=user))
-
-def sadd(commission, oldseat, user):
-	""" Creates the oldseat <oldseat> for <commission> with <user> """
-	s = OldSeat(name=oldseat,
-		 humanName=oldseat.capitalize(),
-		 user=OldKnUser.objects.get(username=user),
-		 group=OldKnGroup.objects.get(name=commission),
-		 description=oldseat)
-	s.save()
-
-def aadd(source, target):
-	""" Creates the alias <source> -> <target> """
-	a = Alias(source=source, target=target)
-	a.save()
-
-def u(username):
-	""" Gets the <OldKnUser> with username <username> """
-	return OldKnUser.objects.get(username=username)
-
-def g(name):
-	""" Gets the <OldKnGroup> with name <name> """
-	return OldKnGroup.objects.get(name=name)
-
-def ml(name):
-	""" Gets the Mailman.MailList.MailList object for the list
-		<name>. (Unlocked) """
-	return Mailman.MailList.MailList(name, False)
-
-def setgp(name, parent):
-	""" Sets the parent of <name> to <parent> """
-	gr = g(name)
-	gr.parent = g(parent)
-	gr.save()
+import kn.leden.entities as es
