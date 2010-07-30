63024f7204cd9cac33c88afe55ca114d136842c7
Bas Westerbaan <bas@westerbaan.name>
2010-07-30 15:57:31 +0200

    Remove oldkngroup_detail view
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/leden/views.py
+++ kn/leden/views.py
@@ -106,30 +106,6 @@ def _institute_detail(request, institute):
 # Unconverted
 # ----------------
 @login_required
-def oldkngroup_detail(request, name):
-	try:
-		group = OldKnGroup.objects.select_related('oldseat_set',
-				'user_set').order_by('humanName').get(name=name)
-	except OldKnGroup.DoesNotExist:
-		raise Http404
-	otherMembers = list()
-	subGroups = filter(lambda x: not x.isHidden,
-			group.oldkngroup_set.order_by('humanName').all())
-	oldseats = list(group.oldseat_set.order_by('humanName').all())
-	oldseat_ids = frozenset(map(lambda x: x.user_id, oldseats))
-	for user in group.user_set.select_related('oldknuser'
-			).order_by('first_name').all():
-		if user.id in oldseat_ids:
-			continue
-		otherMembers.append(user)
-	return render_to_response('leden/oldkngroup_detail.html',
-			{'object': group,
-		 	 'oldseats': oldseats,
-			 'subGroups': subGroups,
-			 'otherMembers': otherMembers},
-			context_instance=RequestContext(request))
-
-@login_required
 def oldknuser_smoel(request, name):
 	try:
 		user = OldKnUser.objects.get(username=name)
