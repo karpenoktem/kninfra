cffdacd0ce5e59e9818519fabcac63729643c6f2
KN Webcie <webcie@karpenoktem.nl>
2010-08-31 19:27:46 +0000

    leden/telefoonlijst: fix problem with encoding
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

--- utils/leden/telefoonlijst.py
+++ utils/leden/telefoonlijst.py
@@ -3,6 +3,10 @@ import _import
 from common import *
 import sys
 
+if sys.stdout.encoding is None:
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+
 for m in args_to_users(sys.argv[1:]):
 	print "%15s%20s%15s" % (m.first_name,
 				m.last_name,
