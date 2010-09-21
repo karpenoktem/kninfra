a4706813ab7085c7a2c3faa120a4df2a614bbe76
Jille Timmermans <jille@karpenoktem.nl>
2010-09-21 21:07:17 +0000

    admin: poll: Allow some filtering for AnswerSet's and Questions
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- kn/poll/admin.py
+++ kn/poll/admin.py
@@ -1,5 +1,13 @@
 from models import Poll, AnswerSet, Answer, Question, Filling
 from django.contrib import admin
 
-for m in (Poll, AnswerSet, Answer, Question):
-	admin.site.register(m)
+class AnswerAdmin(admin.ModelAdmin):
+	list_filter = ('answerSet',)
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_filter = ('poll',)
+
+admin.site.register(Poll)
+admin.site.register(AnswerSet)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Question, QuestionAdmin)
