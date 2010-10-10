ce89bd45055439f8b9ed715a1b89ff9b8c165a34
KN Webcie <webcie@karpenoktem.nl>
2010-10-10 11:05:38 +0000

    Copy setup_virtual_package to kn.base.runtime
    Signed-off-by: KN Webcie <webcie@karpenoktem.nl>

---
+++ kn/base/runtime.py
@@ -0,0 +1,12 @@
+import sys
+import imp
+import os.path
+
+def setup_virtual_package(name, path=os.curdir):
+        """ Sets up a package at the given path with a given
+            name """
+        modulePath = os.path.abspath(path)
+        f, fn, suffix = imp.find_module('__init__',
+                 [modulePath])
+        imp.load_module(name, f, fn, suffix)
+        sys.modules[name].__path__ = [modulePath]
