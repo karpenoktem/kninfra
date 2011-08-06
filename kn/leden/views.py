53e8cb0787c4cc6c6bfe03d1a6e578b160d8e988
Bas Westerbaan <bas@westerbaan.name>
2011-08-06 12:18:28 +0200

    Use relation_cmp_{from,until}
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/leden/views.py
+++ kn/leden/views.py
@@ -49,23 +49,23 @@ def entity_detail(request, name=None, _id=None, type=None):
 
 def _entity_detail(request, e):
 	def _cmp(x,y):
-		r = cmp(x['until'], y['until'])
+		r = Es.relation_cmp_until(x,y)
 		if r: return r
 		r = cmp(x['with'].humanName, y['with'].humanName)
 		if r: return r
 		r = cmp(x['how'].humanName if x['how'] else None,
 			y['how'].humanName if y['how'] else None)
 		if r: return r
-		return cmp(x['from'], y['from'])
+		return Es.relation_cmp_from(x,y)
 	def _rcmp(x,y):
-		r = cmp(x['until'], y['until'])
+		r = Es.relation_cmp_until(x,y)
 		if r: return r
 		r = cmp(x['how'].humanName if x['how'] else None,
 			y['how'].humanName if y['how'] else None)
 		if r: return r
 		r = cmp(x['who'].humanName, y['who'].humanName)
 		if r: return r
-		return cmp(x['from'], y['from'])
+		return Es.relation_cmp_from(x,y)
 	related = sorted(e.get_related(), cmp=_cmp)
 	rrelated = sorted(e.get_rrelated(), cmp=_rcmp)
 	tags = list(e.get_tags())
