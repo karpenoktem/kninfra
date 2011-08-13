bf1c9ed183ac9830b189c15ed7990ed01d871496
Bas Westerbaan <bas@westerbaan.name>
2011-08-13 08:49:35 +0200

    giedo/postfix: make non-mailman group handling an order faster
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/utils/giedo/postfix.py
+++ kn/utils/giedo/postfix.py
@@ -1,23 +1,35 @@
 import logging
 import kn.leden.entities as Es
 from kn.settings import LISTS_MAILDOMAIN
+from kn.leden.date import now
 
-def update_postfix():
-        tbl = dict()
+def update_postfix(giedo):
+        tbl = dict() # the virtual map
+        non_mailman_groups = {}
+        dt_now = now()
+        id2email = {}
+        # handle the straightforward cases
         for e in Es.all():
                 if e.canonical_email is None or e.name is None:
                         continue
+                id2email[e._id] = e.canonical_email
                 for nm in e.other_names:
-                        tbl[str(nm)] = (e.canonical_email,)
+                        tbl["%s@%s" % (nm, MAILDOMAIN)] = (e.canonical_email,)
                 if e.type == 'user':
-                        tbl[str(e.name)] = (e.primary_email,)
+                        tbl[e.canonical_email] = (e.primary_email,)
                 elif e.type == 'group':
                         if e.got_mailman_list:
-                                tbl[str(e.name)] = ('%s@%s' % (str(e.name),
-                                                        LISTS_MAILDOMAIN),)
+                                tbl[e.canonical_email] = ('%s@%s' % (
+                                        str(e.name), LISTS_MAILDOMAIN),)
                         else:
-                                tbl[str(e.name)] = [m.canonical_email
-                                                for m in e.get_members()]
+                                tbl[e.canonical_email] = []
+                                non_mailman_groups[e._id] = e
                 else:
                         logging.warn("postfix: unhandled type: %s" % e.type)
-
+        # handle the non-mailman groups
+        for rel in Es.query_relations(_with=non_mailman_groups.keys(),
+                        _from=dt_now, until=dt_now, how=None):
+                e = non_mailman_groups[rel['with']]
+                email = id2email.get(rel['who'])
+                if email is not None:
+                        tbl[e.canonical_email].append(email)
