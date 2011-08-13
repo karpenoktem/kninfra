c40c5c286c45523ab2f0eb16fae2e262b259eb5d
Bas Westerbaan <bas@westerbaan.name>
2011-08-13 12:41:47 +0200

    daan: implement Mailman creation code
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/utils/daan/mailman.py
+++ kn/utils/daan/mailman.py
@@ -1,8 +1,12 @@
 import logging
+import os
+from subprocess import call
+
+from kn import settings
 
 from kn.utils.mailman import import_mailman
 import_mailman()
-from Mailman import Utils, MailList, UserDesc, Errors
+from Mailman import Utils, MailList, UserDesc, Errors, mm_cfg
 
 def apply_mailman_changes(daan, changes):
         mlo = {}
@@ -15,6 +19,27 @@ def apply_mailman_changes(daan, changes):
                 except Errors.MMUnknownListError:
                         logging.warn("mailman: could not open %s" % l)
                 return False
+        for name, humanName in changes['create']:
+                newlist = os.path.join(settings.MAILMAN_PATH, 'bin/newlist')
+                ret = call([newlist, '-q', name, settings.MAILMAN_DEFAULT_OWNER,
+                                        settings.MAILMAN_DEFAULT_PWD])
+                if ret != 0:
+                        logging.error("bin/newlist failed")
+                        continue
+                # Our custom settings
+                # from: http://karpenoktem.com/wiki/WebCie:Mailinglist
+                ensure_opened(name)
+                ml = mlo[name]
+                ml.send_reminders = 0
+                ml.send_welcome_msg = False
+                ml.max_message_size = 0
+                ml.subscribe_policy = 3
+                ml.unsubscribe_policy = 0
+                ml.private_roster = 2
+                ml.generic_nonmember_action = 0
+                ml.require_explicit_destination = 0
+                ml.max_num_recipients = 0
+                ml.archive_private = 1
         try:
                 for l in changes['add']:
                         if not ensure_opened(l):
