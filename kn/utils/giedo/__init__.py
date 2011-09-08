1c016fc0ba2d9fb70ca125a0ed6983efb7235a0a
Bas Westerbaan <bas@westerbaan.name>
2011-09-08 13:35:04 +0200

    giedo: upgrade from stub
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/utils/giedo/__init__.py
+++ kn/utils/giedo/__init__.py
@@ -5,6 +5,8 @@ import select
 import json
 import os
 
+import kn.leden.entities as Es
+
 from kn.utils.whim import WhimDaemon, WhimClient
 
 from kn import settings
@@ -14,20 +16,39 @@ from kn.utils.giedo.postfix import generate_postfix_map
 from kn.utils.giedo.mailman import generate_mailman_changes
 from kn.utils.giedo.wiki import generate_wiki_changes
 from kn.utils.giedo.forum import generate_forum_changes
+from kn.utils.giedo.unix import generate_unix_map
 
 class Giedo(WhimDaemon):
         def __init__(self):
                 super(Giedo, self).__init__(settings.GIEDO_SOCKET)
                 self.daan = WhimClient(settings.DAAN_SOCKET)
+                self.cilia = WhimClient(settings.CILIA_CONN_ADDR, family='tcp')
 
         def handle(self, d):
-                # For now, be stupid, and update what we are developing
-                #update_db(self)
-                #self.daan.send({'type': 'postfix',
-                #                'map': generate_postfix_map(self)})
-                #self.daan.send({'type': 'mailman',
-                #                'changes': generate_mailman_changes(self)})
-                #self.daan.send({'type': 'wiki',
-                #                'changes': generate_wiki_changes(self)})
-                self.daan.send({'type': 'forum',
+                if d['type'] == 'sync':
+                        print 'updatedb'
+                        update_db(self)
+                        self.daan.send({'type': 'postfix',
+                                'map': generate_postfix_map(self)})
+                        self.daan.send({'type': 'mailman',
+                                'changes': generate_mailman_changes(self)})
+                        self.daan.send({'type': 'wiki',
+                                'changes': generate_wiki_changes(self)})
+                        self.daan.send({'type': 'forum',
                                 'changes': generate_forum_changes(self)})
+                        self.cilia.send({'type': 'unix',
+                                 'map': generate_unix_map(self)})
+                elif d['type'] == 'setpass':
+                        u = Es.by_name(d['user'])
+                        if u is None:
+                                return {'error': 'no such user'}
+                        u = u.as_user()
+                        if not u.check_password(d['oldpass']):
+                                return {'error': 'wrong old password'}
+                        u.set_password(d['newpass'])
+                        d2 = {'type': 'setpass',
+                              'user': d['user'],
+                              'pass': d['newpass']}
+                        self.daan.send(d2)
+                        self.cilia.send(d2)
+                        return {'success': True}
