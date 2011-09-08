2c17f9cd4b830e8ccdff7a3fa229d54a9ba29379
Bas Westerbaan <bas@westerbaan.name>
2011-09-08 12:23:24 +0200

    base: add _random with pseudo_randstr
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ kn/base/_random.py
@@ -0,0 +1,11 @@
+import random
+
+ALPHA = 'qwertyuiopasdfghjklzxcvbnm'
+NUM = '1234567890'
+ALPHANUMUL = ALPHA + ALPHA.upper() + NUM
+
+def pseudo_randstr(l=12, cs=ALPHANUMUL):
+	ret = ''
+	for i in xrange(l):
+		ret += cs[random.randint(0, len(cs)-1)]
+	return ret
