da6618481c76fcfccff3ccda5f7f2ac69d576272
Jille Timmermans <jille@karpenoktem.nl>
2011-09-13 17:45:46 +0200

    daan/forum: Don't reopen MySQL-cursor for every action and COMMIT only once
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- kn/utils/daan/forum.py
+++ kn/utils/daan/forum.py
@@ -24,8 +24,8 @@ def apply_forum_changes(daan, changes):
         creds = settings.FORUM_MYSQL_CREDS
         dc = MySQLdb.connect(creds[0], user=creds[1], passwd=creds[2],
                                 db=creds[3])
+        c = dc.cursor()
         for user, realname, email in changes['add']:
-                c = dc.cursor()
                 q = """INSERT INTO users (`username`,
                                           `password`,
                                           `email`,
@@ -33,16 +33,10 @@ def apply_forum_changes(daan, changes):
                                           `registered`)
                         VALUES (%s, %s, %s, %s, %s);"""
                 c.execute(q, user, '37', email, realname, int(time.time()))
-                c.execute("COMMIT;")
-                c.close()
         for user, realname in changes['update-realname']:
-                c = dc.cursor()
                 c.execute("UPDATE users SET realname=%s WHERE username=%s", realname, user)
-                c.execute("COMMIT;")
-                c.close()
         for user in changes['remove']:
-                c = dc.cursor()
                 c.execute("DELETE FROM `users` WHERE `username`=%s", user)
-                c.execute("COMMIT;")
-                c.close()
+        c.execute("COMMIT;")
+        c.close()
         dc.close()
