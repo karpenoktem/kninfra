35124561fd4a6d449c64451b0bde280353f88e06
Bas Westerbaan <bas@westerbaan.name>
2011-09-14 13:55:20 +0200

    Update utils/membership
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- utils/leden/membership.py
+++ utils/leden/membership.py
@@ -2,30 +2,36 @@ import _import
 
 import sys
 from common import *
-from kn.leden.models import OldKnUser, OldKnGroup
+import kn.leden.entities as Es
 
 users = dict()
 if len(sys.argv) == 1:
-	_users = OldKnUser.objects.all()
+	_users = Es.users()
 else:
 	_users = args_to_users(sys.argv[1:])
 
 for m in _users:
-	users[m.username] = set()
+	users[str(m.name)] = set()
 
 i = 0
 while True:
 	i += 1
-	try:
-		g = OldKnGroup.objects.get(name='leden%s'%i)
-	except OldKnGroup.DoesNotExist:
-		break
-	for m in g.user_set.all():
-		if m.username in users:
-			users[m.username].add(i)
+        g = Es.by_name('leden%s' % i)
+        if g is None:
+                break
+	for m in g.get_members():
+		if str(m.name) in users:
+			users[str(m.name)].add(i)
 nyears = i - 1
+N = 0
 
 for m, ys in users.iteritems():
+        N += 1
+        if N % 20 == 0:
+                print "%15s" % '',
+                for y in xrange(1, nyears + 1):
+                        print y,
+                print
 	print "%15s" % m,
 	for y in xrange(1, nyears + 1):
 		print '*' if y in ys else ' ',
