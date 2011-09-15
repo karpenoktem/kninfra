827c7418971e50d2992fa9eddd636a96a6776367
Bas Westerbaan <bas@westerbaan.name>
2011-09-15 17:00:34 +0200

    leden.date: add date_to_dt
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/leden/date.py
+++ kn/leden/date.py
@@ -3,3 +3,6 @@ import datetime
 # TODO Cache this in a proper way
 def now():
         return datetime.datetime.now()
+
+def date_to_dt(d):
+        return datetime.datetime.combine(d, datetime.time())
