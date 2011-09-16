433fab6cce4a1fb6b2e103aad6acba94ae2fdad4
Bas Westerbaan <bas@westerbaan.name>
2011-09-16 23:10:07 +0200

    shell: add del_rel and add_rel
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- utils/shell.py
+++ utils/shell.py
@@ -4,4 +4,39 @@ try:
 except ImportError:
 	pass
 
-import kn.leden.entities as es
+import time
+import datetime
+import kn.leden.entities as Es
+from kn.leden.mongo import _id
+
+def del_rel(who, _with, how):
+        """ Removes a relation given by names.
+
+        For instance: del_rel('giedo', 'leden', None) """
+        who = Es.id_by_name(who)
+        _with = Es.id_by_name(_with)
+        how = Es.ecol.find_one({'sofa_suffix': how})['_id'] \
+                        if how is not None else None
+        Es.rcol.remove({'who': who,
+                        'with': _with,
+                        'how': how})
+
+def add_rel(who, _with, how, _from, until):
+        """ Adds a relation given by strings.
+
+        For instance: add_rel('giedo', 'bestuur', 'voorzitter', '2004-09-01',
+                                '2006-09-01') """
+        who = Es.id_by_name(who)
+        _with = Es.id_by_name(_with)
+        how = Es.ecol.find_one({'sofa_suffix': how})['_id'] \
+                        if how is not None else None
+        _from = str_to_date(_from) if _from is not None else None
+        until = str_to_date(until) if until is not None else None
+        Es.rcol.insert({'who': who,
+                        'with': _with,
+                        'how': how,
+                        'from': _from,
+                        'until': until})
+
+def str_to_date(s):
+        return datetime.datetime(*time.strptime(s, '%Y-%m-%d')[:3])
