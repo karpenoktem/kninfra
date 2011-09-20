be26e06b64f20d3276e173653989b054db20cc5c
Bas Westerbaan <bas@westerbaan.name>
2011-09-21 00:33:40 +0200

    subscription: small fixes to event_detail
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- kn/subscriptions/views.py
+++ kn/subscriptions/views.py
@@ -58,16 +58,15 @@ def event_detail(request, name):
 		request.user.push_message(
 				"Je bent aangemeld en moet "+\
 					"nu %s euro betalen" % event.cost)
-	if request.user.is_related_with(event.owner):
-		subscrlist = tuple(event.get_subscriptions())
-		subscrcount_debit = len([s for s in subscrlist if s.debit != 0])
-        else:
-		subscrlist = None
-		subscrcount_debit = None
-	return render_to_response('subscriptions/event_detail.html',
-			{'object': event,
-			 'user': request.user,
-			 'subscrlist': subscrlist,
-			 'subscrcount_debit': subscrcount_debit,
-			 'subscription': subscription},
+        ctx = {'object': event,
+               'user': request.user,
+               'subscription': subscription}
+        if event.has_read_access(request.user):
+                subscrlist = tuple(event.get_subscriptions())
+	        ctx.update({
+                        'subscrlist': subscrlist,
+                        'subscrcount_debit': len([s for s in subscrlist
+                                                        if s.debit != 0]),
+                        'subscrlist_count': len(subscrlist)})
+	return render_to_response('subscriptions/event_detail.html', ctx,
 			context_instance=RequestContext(request))
