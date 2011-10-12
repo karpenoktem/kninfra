d19445a7a91774d7cee4a0e9e2a077d6d4008969
Jille Timmermans <jille@karpenoktem.nl>
2011-10-12 23:53:19 +0200

    Util 'add-borrel.py'
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

---
+++ utils/planning/add-borrel.py
@@ -0,0 +1,28 @@
+import _import
+import datetime
+import sys
+
+import kn.leden.entities as Es
+from kn.settings import DT_MIN, DT_MAX
+from kn.leden.mongo import _id
+from kn.planning.entities import Pool, Worker, Vacancy
+
+def hm2s(hours, minutes=0):
+	return (hours * 60 + minutes) * 60
+
+day = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+pool = Pool.by_name('tappers')
+
+vacancies = list()
+for period in [[hm2s(20, 30), hm2s(23)], [hm2s(23), hm2s(25)], [hm2s(25), hm2s(28)]]:
+	v = Vacancy({
+		'name': 'Borrel',
+		'date': day,
+		'begin': day + datetime.timedelta(seconds=period[0]),
+		'end': day + datetime.timedelta(seconds=period[1]),
+		'pool': _id(pool),
+		'assignee': None,
+	})
+	print v._data
+	v.assignee = None
+	v.save()
