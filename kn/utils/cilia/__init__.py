df4b9ef005550c3321857475f8927b29581b7b45
Jille Timmermans <jille@karpenoktem.nl>
2011-10-19 12:04:38 +0200

    cilia: First version with samba-support
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- kn/utils/cilia/__init__.py
+++ kn/utils/cilia/__init__.py
@@ -16,11 +16,18 @@ class Cilia(WhimDaemon):
         def __init__(self):
                 super(Cilia, self).__init__(settings.CILIA_SOCKET)
                 self.unix_lock = threading.Lock()
+                self.samba_lock = threading.Lock()
 
         def handle(self, d):
                 if d['type'] == 'unix':
+                        # XXX decide where we will call this as this is a bit
+                        # ugly
+                        with self.samba_lock:
+                                set_samba_map(self, d['map'])
                         with self.unix_lock:
                                 return set_unix_map(self, d['map'])
                 elif d['type'] == 'setpass':
+                        with self.samba_lock:
+                                return samba_setpass(self, d['user'], d['pass'])
                         with self.unix_lock:
                                 return unix_setpass(self, d['user'], d['pass'])
