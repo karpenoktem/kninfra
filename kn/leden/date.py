3bdeafddf062a60160285f6e24be45d4b1d27be2
Bram Westerbaan <bram@westerbaan.name>
2011-11-16 00:03:41 +0100

    adds simple ~1-second caching for leden.date.now()

--- kn/leden/date.py
+++ kn/leden/date.py
@@ -1,9 +1,26 @@
 # vim: et:sta:bs=2:sw=4:
 import datetime
+import time
+import threading
 
-# TODO Cache this in a proper way
 def now():
-        return datetime.datetime.now()
+    return sclock.now
+
+class Clock(threading.Thread):
+    def __init__(self, interval):
+        super(Clock,self).__init__()
+        self.interval = interval
+        self.now = datetime.datetime.now()
+        self.daemon = True
+        # ^ Prevents this thread from keeping the program alive.
+
+    def run(self):
+        while(True):
+            time.sleep(self.interval)
+            self.now = datetime.datetime.now()
+
+sclock = Clock(1)
+sclock.start()
 
 def date_to_dt(d):
-        return datetime.datetime.combine(d, datetime.time())
+    return datetime.datetime.combine(d, datetime.time())
