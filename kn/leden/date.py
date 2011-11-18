8213d90d12b675483695c30a3e590d1be0825cd1
Bram Westerbaan <bram@westerbaan.name>
2011-11-18 14:34:49 +0100

    leden.date.now():  verwijdert caching
    datetime.now() blijkt niet langzaam te zijn,
    waarschijnlijk gettimeofday() een virtuele syscall is.

--- kn/leden/date.py
+++ kn/leden/date.py
@@ -1,26 +1,9 @@
 # vim: et:sta:bs=2:sw=4:
 import datetime
 import time
-import threading
 
 def now():
-    return sclock.now
-
-class Clock(threading.Thread):
-    def __init__(self, interval):
-        super(Clock,self).__init__()
-        self.interval = interval
-        self.now = datetime.datetime.now()
-        self.daemon = True
-        # ^ Prevents this thread from keeping the program alive.
-
-    def run(self):
-        while(True):
-            time.sleep(self.interval)
-            self.now = datetime.datetime.now()
-
-sclock = Clock(1)
-sclock.start()
+    return datetime.datetime.now()
 
 def date_to_dt(d):
     return datetime.datetime.combine(d, datetime.time())
