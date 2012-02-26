633efeaeff6f7d66501e30057b676ede66bd1eb2
Bas Westerbaan <bas@westerbaan.name>
2012-02-26 01:28:44 +0100

    utils: rewrite poll/all-poll-results to Mongo
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

--- utils/poll/all-poll-results.py
+++ utils/poll/all-poll-results.py
@@ -1,47 +1,20 @@
 # vim: et:sta:bs=2:sw=4:
 import _import
-from django import db
-from kn.poll.models import *
-from django.contrib.auth.models import User
+
+import kn.poll.entities as poll_Es
 
 def main():
-    plut = dict()
-    qlut = dict()
-    alut = dict()
-    ulut = dict()
-    for q in Question.objects.all():
-        if not q.poll_id in plut:
-            plut[q.poll_id] = list()
-        plut[q.poll_id].append(q)
-    for f in Filling.objects.all():
-        if not f.question_id in qlut:
-            qlut[f.question_id] = list()
-        qlut[f.question_id].append(f)
-    for a in Answer.objects.all():
-        alut[a.pk] = a
-    for u in User.objects.all():
-        ulut[u.pk] = u
-    for poll in Poll.objects.all():
-        print poll
-        if not poll.pk in plut:
-            continue
-        for q in plut[poll.pk]:
-            lut = dict()
-            if not q.id in qlut:
-                continue
-            for f in qlut[q.id]:
-                if not f.answer_id in lut:
-                    lut[f.answer_id] = list()
-                lut[f.answer_id].append(f.user_id)
-            tmp = sorted(lut.iteritems(),
-                     cmp=lambda x,y: cmp(len(y[1]),
-                             len(x[1])))
-            if len(tmp) == 0:
-                continue
-            print " %s" %q
-            for a, us in tmp:
-                us = map(lambda x: ulut[x].username, us)
-                print '  %-25s %s %s' % (alut[a], len(us), ', '.join(us))
+    for p in poll_Es.all_polls():
+        print p.humanName
+        counts = [[0] * (len(q[1]) + 1) for q in p.questions]
+        for f in p.fillings():
+            for n, a in enumerate(f.answers):
+                counts[n][len(p.questions[n][1])+1 if a is None else a] += 1
+        for n, q in enumerate(p.questions):
+            print ' %s' % q[0]
+            for m, a in enumerate(q[1]):
+                print '  %-50s %s' % (a, counts[n][m])
+        print
 
 if __name__ == '__main__':
     main()
