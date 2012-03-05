4da6f8c7d8018b22be6e3bf5a6340b674026cb90
Jille Timmermans <jille@karpenoktem.nl>
2012-03-05 13:31:35 +0100

    planning: add-borrel.py: "Kind" ook invullen.
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- utils/planning/add-borrel.py
+++ utils/planning/add-borrel.py
@@ -26,7 +26,8 @@ day = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
 
 e = Event({
     'name': 'Borrel',
-    'date': day })
+    'date': day,
+    'kind': 'borrel' })
 e.save()
 
 for _type, periods in typePeriods.items():
