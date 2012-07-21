8a0a9e9dac5800866fd869d5259fb03930d6593e
Bas Westerbaan <bas@westerbaan.name>
2012-07-21 16:36:00 +0200

    Add anonymized-dump/restore
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ utils/anonymized-dump/restore.py
@@ -0,0 +1,48 @@
+import _import
+
+from kn.leden.mongo import db
+
+import bson
+import pymongo
+
+import kn.leden.entities as Es
+import kn.moderation.entities as Es_mod
+import kn.planning.entities as Es_plan
+import kn.poll.entities as Es_poll
+import kn.reglementen.entities as Es_regl
+import kn.subscriptions.entities as Es_subscr
+
+def main():
+    print 'Are you sure you want to run this?'
+    return
+    db.command('dropDatabase')
+    print 'Creating indices'
+    print ' leden'
+    Es.ensure_indices()
+    print ' moderation'
+    Es_mod.ensure_indices()
+    print ' planning'
+    Es_plan.ensure_indices()
+    print ' poll'
+    Es_poll.ensure_indices()
+    print ' regl'
+    Es_regl.ensure_indices()
+    print ' subscriptions'
+    Es_subscr.ensure_indices()
+    print
+    print 'Restoring data'
+    print ' entities'
+    for e in bson.decode_all(open('entities.bsons').read()):
+        db['entities'].save(e)
+    print ' relations'
+    for e in bson.decode_all(open('relations.bsons').read()):
+        db['relations'].save(e)
+        print e
+    print ' events'
+    for e in bson.decode_all(open('events.bsons').read()):
+        db['events'].save(e)
+
+if __name__ == '__main__':
+    main()
+
+# vim: et:sta:bs=2:sw=4:
