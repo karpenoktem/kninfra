f4fdb635c920ac9d7b9c1762cc0d443268e56b8e
Jille Timmermans <jille@karpenoktem.nl>
2012-08-31 12:58:48 +0200

    Remove argument to lambda-function
    Signed-off-by: Jille Timmermans <jille@karpenoktem.nl>

--- kn/leden/forms.py
+++ kn/leden/forms.py
@@ -43,7 +43,7 @@ class AddGroupForm(forms.Form):
     parent = EntityChoiceField(label="Parent",
             choices=filter(lambda x: not x.is_virtual, Es.groups()),
             sort_choices=True,
-            initial=lambda x: str(Es.by_name('secretariaat')._id))
+            initial=lambda: str(Es.by_name('secretariaat')._id))
 
 class ChangePasswordForm(forms.Form):
     old_password = forms.CharField(widget=forms.PasswordInput())
