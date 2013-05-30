d793bca692eb7b814da6b076aef2999c69e8f7f8
Bas Westerbaan <bas@karpenoktem.nl>
2013-05-30 14:09:29 +0200

    [django 1.4] fix auth context processor

--- kn/settings.example.py
+++ kn/settings.example.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = (
     'kn.planning',
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
