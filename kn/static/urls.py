67e9c18d8f897f62790bdc3be6e849c4c9058485
Felix Janssen <thundur@thundur.com>
2014-07-11 23:24:11 +0200

    Built slideshow (+jquery update)

--- kn/static/urls.py
+++ kn/static/urls.py
@@ -6,9 +6,13 @@ import django.views.generic as generic
 import django.views.generic.simple
 from kn.base.views import direct_to_folder
 
+from views import slideshow_images
+
 urlpatterns = patterns('',
     url(r'^(?:(?:home|default)/?)?$', generic.simple.direct_to_template,
-            {'template': 'static/home.html'}, name='home'),
+        {'template': 'static/home.html', 'extra_context':
+            {'slideshow_images': slideshow_images}},
+        name='home'),
     # TODO we have to specify a separate url entry to make the reverse URL work
     #      for pages with several names.  Is there a way to do this without
     #      the duplication.
--- kn/static/views.py
+++ kn/static/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.conf import settings
+
+import os.path
+
+# initialize the list of slideshow images for home.html
+path = os.path.join(settings.MEDIA_ROOT, 'static/slideshow')
+slideshow_images = []
+for fn in os.listdir(path):
+    slideshow_images.append(os.path.join(settings.MEDIA_URL, 'static/slideshow', fn))
+
+# vim: et:sta:bs=2:sw=4:
