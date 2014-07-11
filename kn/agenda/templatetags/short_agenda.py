3f67a71f77f7460aff0d81d81d0c8cfd60d9dda7
Felix Janssen <felix@karpenoktem.nl>
2014-07-11 21:51:39 +0200

    Aantal agenda-items op de homepage op 10 gelimiteerd. Meer-linkje tonen als er meer dan 10 zijn.

--- kn/agenda/templatetags/short_agenda.py
+++ kn/agenda/templatetags/short_agenda.py
@@ -2,7 +2,9 @@ from django import template
 import kn.agenda.entities as Es_a
 
 register = template.Library()
+max_items = 10;
 
 @register.inclusion_tag('agenda/short_agenda.html')
 def show_short_agenda():
-        return {'short_agenda': Es_a.all()}
+        Es_limited = list(Es_a.all(limit=max_items + 1));
+        return {'short_agenda': Es_limited[0:max_items], 'has_more': len(Es_limited) > max_items}
