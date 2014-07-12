3a557bf99ec019fdd323f677c67e00e929638598
Bas Westerbaan <bas@westerbaan.name>
2014-07-12 14:24:14 +0200

    Rmove stray locale calls

--- kn/utils/giedo/siteagenda.py
+++ kn/utils/giedo/siteagenda.py
@@ -1,13 +1,5 @@
-import locale
-
 from kn.agenda.fetch import fetch
 from kn.agenda.entities import update
-
-try:
-    # Nederlandse afkortingen voor data
-    locale.setlocale(locale.LC_ALL, 'nl_NL')
-except Exception:
-    pass
 
 def update_site_agenda(giedo):
     update(fetch())
