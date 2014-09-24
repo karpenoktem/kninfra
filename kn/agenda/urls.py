075f789c698f60c12858e3d8042648e90d6e9a2c
Ayke van Laethem <ayke@karpenoktem.nl>
2014-09-24 23:58:28 +0200

    Make CSS files static
    CSS files do not have to be Django templates.

--- kn/agenda/urls.py
+++ kn/agenda/urls.py
@@ -8,10 +8,6 @@ urlpatterns = patterns('',
     url(r'^agenda/?$', views.agenda, name='agenda'),
     url(r'^ledenmail-template/?$', views.ledenmail_template,
                     name='ledenmail-template'),
-    url(r'^styles/agenda/$',
-        generic.simple.direct_to_template,
-        {'template':'agenda/agenda.css',
-         'mimetype':'text/css'}, name='style-agenda'),
 )
 
 # vim: et:sta:bs=2:sw=4:
