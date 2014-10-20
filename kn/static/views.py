d8ce59e9e361c8623d528a338e354b73d4112338
Bas Westerbaan <bas@westerbaan.name>
2014-10-20 20:06:09 +0200

    improve upon 1ab18fc

--- kn/static/views.py
+++ kn/static/views.py
@@ -3,7 +3,6 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 
 import os
-import glob
 
 _slideshow_images = None
 
@@ -12,9 +11,7 @@ def home(request):
     if _slideshow_images is None:
         _slideshow_images = []
         path = os.path.join(settings.MEDIA_ROOT, 'static/slideshow')
-        files = os.listdir(path)
-        files.sort()
-        for fn in files:
+        for fn in sorted(os.listdir(path)):
             _slideshow_images.append(os.path.join(settings.MEDIA_URL,
                                         'static/slideshow', fn))
     return render_to_response('static/home.html',
