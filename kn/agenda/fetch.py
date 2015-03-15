58c5533ac14ec2447c087a87e3cbcf76d43715ca
Bas Westerbaan <bas@karpenoktem.nl>
2015-03-15 22:51:54 +0100

    style

--- kn/agenda/fetch.py
+++ kn/agenda/fetch.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 
-from iso8601 import parse_date
 import kn.settings as settings
+
 import json
 import datetime
+import httplib2
+
+from iso8601 import parse_date
 
 # pip install google-api-python-client
 from oauth2client.client import SignedJwtAssertionCredentials
 from googleapiclient.discovery import build
-import httplib2
 
 # How to configure the agenda:
 #
@@ -31,10 +33,11 @@ import httplib2
 
 def get_credentials():
     key = json.loads(open(settings.GOOGLE_OAUTH2_KEY, 'r').read())
-    return SignedJwtAssertionCredentials(service_account_name=key['client_email'],
-                                         private_key=key['private_key'],
-                                         scope='https://www.googleapis.com/auth/calendar.readonly',
-                                         user_agent='Karpe Noktem agenda fetcher')
+    return SignedJwtAssertionCredentials(
+                    service_account_name=key['client_email'],
+                    private_key=key['private_key'],
+                    scope='https://www.googleapis.com/auth/calendar.readonly',
+                    user_agent='Karpe Noktem agenda fetcher')
 
 def parse_item_date(date):
     # dateTime: specific time
@@ -48,19 +51,17 @@ def fetch():
     h = httplib2.Http()
     credentials = get_credentials()
     credentials.authorize(h)
-
     timeMin = datetime.datetime.utcnow().date().isoformat() + 'T00:00:00Z'
-
     cal = build('calendar', 'v3', http=h)
-    request = cal.events().list(calendarId=settings.GOOGLE_CALENDAR_ID, timeMin=timeMin, fields='items(summary,description,start,end)')
+    request = cal.events().list(calendarId=settings.GOOGLE_CALENDAR_ID,
+                                timeMin=timeMin,
+                                fields='items(summary,description,start,end)')
     response = request.execute()
-
     agenda = []
     for item in response['items']:
         agenda.append((item['summary'], item['description'],
                        parse_item_date(item['start']),
                        parse_item_date(item['end'])))
-
     return agenda
 
 
