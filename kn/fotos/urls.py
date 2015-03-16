95657044f1ec7ea6f19543ca31e0f9a71a25e575
Ayke van Laethem <ayke@karpenoktem.nl>
2015-03-16 15:05:18 +0100

    Use feedback from pull request

--- kn/fotos/urls.py
+++ kn/fotos/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls.defaults import *
-from django.shortcuts import redirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 from kn.fotos import views
 from kn.fotos import api
 
 urlpatterns = patterns('',
+        # backwards compatibility with knfotos
+        url(r'^fotos/view\.php$', views.compat_view),
+        url(r'^fotos/foto\.php$', views.compat_foto),
+        url(r'^fotos/index\.php$', RedirectView.as_view(
+            url=reverse_lazy('fotos', kwargs={'path':''}),
+            query_string=True)),
+
         # TODO add fallback for old foto links
         # TODO change wiki links, etc.
         url(r'^foto/admin/?$',
@@ -13,8 +21,6 @@ urlpatterns = patterns('',
             views.fotoadmin_create_event, name='fotoadmin-create-event'),
         url(r'^fotos/api/?$',
             api.view, name='fotos-api'),
-        url(r'^fotos/view\.php$', views.compat_view),
-        url(r'^fotos/foto\.php$', views.compat_foto),
         url(r'^fotos/(?P<path>.*)$',
             views.fotos, name='fotos'),
         # NOTE keep up to date with media/fotos.js
