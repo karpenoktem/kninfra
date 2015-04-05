9cba4e62e7c0f2effb9f158b185341d97940d0f3
Bas Westerbaan <bas@karpenoktem.nl>
2015-04-05 22:12:17 +0200

    fotos.roots: temporarily disable wolk

--- kn/fotos/roots.py
+++ kn/fotos/roots.py
@@ -3,7 +3,9 @@ from kn import settings
 from collections import namedtuple
 foto_root = namedtuple('foto_root', ('base', 'between'))
 
-FOTO_ROOTS = {'home': foto_root(settings.USER_DIRS, 'fotos'),
-              'wolk': foto_root(settings.WOLK_DATA_PATH, 'files/Photos')}
+FOTO_ROOTS = {
+              'home': foto_root(settings.USER_DIRS, 'fotos'),
+              #'wolk': foto_root(settings.WOLK_DATA_PATH, 'files/Photos'),
+              }
 
 # vim: et:sta:bs=2:sw=4:
