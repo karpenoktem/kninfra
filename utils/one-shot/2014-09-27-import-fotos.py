739d3b7060da2a3083d52d49d26b52a55c852205
Bas Westerbaan <bas@karpenoktem.nl>
2015-04-05 22:14:40 +0200

    oneshot: import foto's bug

--- utils/one-shot/2014-09-27-import-fotos.py
+++ utils/one-shot/2014-09-27-import-fotos.py
@@ -29,6 +29,8 @@ def main():
     c.execute("SELECT id, name, path, humanname, visibility, description "
                         + "FROM fa_albums ORDER BY path")
     for oldId, name, path, humanName, visibility, description in c.fetchall():
+        if path and not path.endswith('/'):
+            continue
         if fEs.by_oldId('album', oldId) is not None:
             continue
 
