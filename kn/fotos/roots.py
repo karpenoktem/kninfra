5df98606df8666daa0daec55b0cc1f62a6997305
Ayke van Laethem <ayke@karpenoktem.nl>
2015-04-07 16:36:25 +0200

    Add 'fotos' directory, like with the /home photo directory

--- kn/fotos/roots.py
+++ kn/fotos/roots.py
@@ -5,6 +5,7 @@ foto_root = namedtuple('foto_root', ('base', 'between'))
 
 FOTO_ROOTS = {
               'home': foto_root(settings.USER_DIRS, 'fotos'),
+              'wolk_fotos': foto_root(settings.WOLK_DATA_PATH, 'files/fotos'),
               'wolk_photos': foto_root(settings.WOLK_DATA_PATH, 'files/photos'),
               'wolk_Photos': foto_root(settings.WOLK_DATA_PATH, 'files/Photos'),
               }
