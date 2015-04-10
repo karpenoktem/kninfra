5b982f24ef5f136f13daa49a65201c5dd014bd5f
Ayke van Laethem <ayke@karpenoktem.nl>
2015-04-10 23:44:47 +0200

    Use common 'leden_api' function instead of doing it ourself

--- kn/leden/media/entity_detail.js
+++ kn/leden/media/entity_detail.js
@@ -2,14 +2,12 @@
 
 function api_set_property(id, property, value) {
 	if (value === null) return;
-	$.post(leden_api_url, {
-			csrfmiddlewaretoken: csrf_token,
-			data: JSON.stringify({
-				action: 'entity_set_property',
-				id: id,
-				property: property,
-				value: value})
-		}, function(data) {
+	leden_api({
+			action: 'entity_set_property',
+			id: id,
+			property: property,
+			value: value},
+		function(data) {
 			if(data.ok) window.location.reload();
 			else alert(data.error);
 		});
