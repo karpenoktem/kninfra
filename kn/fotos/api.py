karpenoktem/kninfra

commit 113f2875fcb7382e1c4957d57332c97f10260bc6

Calculate new thumb size *after* rotation

kn/fotos/api.py
@@ -136,8 +136,6 @@ def _set_metadata(data, request):
         tags = data['tags']
         entity.set_tags(tags, save=True)
 
-        result['thumbnailSize'] = entity.get_cache_size('thumb')
-
     if entity._type == 'foto':
         if 'rotation' not in data:
             return {'error': 'missing rotation attribute'}
@@ -149,6 +147,8 @@ def _set_metadata(data, request):
         entity.set_rotation(rotation, save=False)
 
         result['largeSize'] = entity.get_cache_size('large')
+
+    result['thumbnailSize'] = entity.get_cache_size('thumb')
 
     entity.set_title(title, save=False)
 

commit 1c7db1a0ed58f9701632e07387ec2d1230940bc0

Fix bug in album editor introduced in 113f2875

kn/fotos/api.py
@@ -117,6 +117,18 @@ def _set_metadata(data, request):
 
     result = {'Ok': True}
 
+    if entity._type == 'foto':
+        if 'rotation' not in data:
+            return {'error': 'missing rotation attribute'}
+        if not isinstance(data['rotation'], int):
+            return {'error': 'rotation should be a number'}
+        rotation = data['rotation']
+        if rotation not in [0, 90, 180, 270]:
+            return {'error': 'rotation is not valid'}
+        entity.set_rotation(rotation, save=False)
+
+        result['largeSize'] = entity.get_cache_size('large')
+
     if entity._type in ['foto', 'video']:
         if 'description' not in data:
             return {'error': 'missing description attribute'}
@@ -136,19 +148,7 @@ def _set_metadata(data, request):
         tags = data['tags']
         entity.set_tags(tags, save=True)
 
-    if entity._type == 'foto':
-        if 'rotation' not in data:
-            return {'error': 'missing rotation attribute'}
-        if not isinstance(data['rotation'], int):
-            return {'error': 'rotation should be a number'}
-        rotation = data['rotation']
-        if rotation not in [0, 90, 180, 270]:
-            return {'error': 'rotation is not valid'}
-        entity.set_rotation(rotation, save=False)
-
-        result['largeSize'] = entity.get_cache_size('large')
-
-    result['thumbnailSize'] = entity.get_cache_size('thumb')
+        result['thumbnailSize'] = entity.get_cache_size('thumb')
 
     entity.set_title(title, save=False)
 