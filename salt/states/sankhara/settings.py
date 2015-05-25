4a70638077ea1f767ae7e590117580d978874f78
Bas Westerbaan <bas@westerbaan.name>
2015-05-25 14:03:03 +0200

    salt: fix: use the right secrets

--- salt/states/sankhara/settings.py
+++ salt/states/sankhara/settings.py
@@ -2,12 +2,12 @@ CHUCK_NORRIS_HIS_SECRET = '{{ pillar['secrets']['chucknorris'] }}'
 SECRET_KEY = '{{ pillar['secrets']['django_secret_key'] }}'
 ALLOWED_API_KEYS = ('{{ pillar['secrets']['apikey'] }}',)
 MAILMAN_DEFAULT_PASSWORD = '{{ pillar['secrets']['mailman_default'] }}'
-WIKI_MYSQL_SECRET = ('localhost', 'wiki',
-                        '{{ pillar['secrets']['mysql_infra_wiki'] }}', 'wiki')
-FORUM_MYSQL_SECRET = ('localhost', 'forum',
-                        '{{ pillar['secrets']['mysql_infra_forum'] }}', 'forum')
-WOLK_MYSQL_SECRET = ('localhost', 'wolk',
-                        '{{ pillar['secrets']['mysql_infra_wolk'] }}', 'wolk')
+WIKI_MYSQL_SECRET = ('localhost', 'giedo',
+                        '{{ pillar['secrets']['mysql_giedo'] }}', 'wiki')
+FORUM_MYSQL_SECRET = ('localhost', 'giedo',
+                        '{{ pillar['secrets']['mysql_giedo'] }}', 'forum')
+WOLK_MYSQL_SECRET = ('localhost', 'giedo',
+                        '{{ pillar['secrets']['mysql_giedo'] }}', 'wolk')
 LDAP_PASS = '{{ pillar['secrets']['ldap_infra'] }}'
 DOMAIN = '{{ grains['fqdn'] }}'
 
