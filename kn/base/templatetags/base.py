55215cd5413f32489a6b6f303c372f74a0424370
Bas Westerbaan <bas@westerbaan.name>
2015-07-05 11:20:45 +0200

    header: missing "()"

--- kn/base/templatetags/base.py
+++ kn/base/templatetags/base.py
@@ -54,7 +54,7 @@ def header(context):
                     if fn != '.keep']
         private.extend(public)
         _header_images = {'public': public, 'private': private}
-    if not 'user' in context or not context['user'].is_authenticated:
+    if not 'user' in context or not context['user'].is_authenticated():
         pool = _header_images['public']
     else:
         pool = _header_images['private']
