a1e865bf714a627e3ce6465963f4e806450fcb21
Ayke van Laethem <ayke@karpenoktem.nl>
2015-10-09 19:14:17 +0200

    Do not fail on absent wolk settings

--- kn/utils/cilia/wolk.py
+++ kn/utils/cilia/wolk.py
@@ -8,6 +8,9 @@ import subprocess
 from django.conf import settings
 
 def wolk_setpass(cilia, user, passwd):
+    if hasattr(settings, 'WOLK_PATH'):
+        logging.warning('wolk: no path available, skipping')
+        return None
     wolk_script = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                     'wolk.php')
     proc = subprocess.Popen(['sudo', '-u', settings.WOLK_USER, 'php',
