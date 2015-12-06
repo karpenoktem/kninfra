bda21cc0db74c20e705e2c69a59a38621d974df1
Bas Westerbaan <bas@karpenoktem.nl>
2015-12-06 15:25:58 +0100

    oneshot: add anonymized uithschrijven script

---
+++ utils/one-shot/2015-08-31-uitschrijven.py
@@ -0,0 +1,21 @@
+import _import
+import datetime
+import kn.leden.entities as Es
+
+def main():
+    for n in []:
+        e = Es.by_name(n)
+        print n
+        for rel in e.get_related(_from=Es.now(), until=Es.now()):
+            grp = rel['with']
+            if grp.is_virtual: continue
+            if str(grp.name) == 'nibbana': continue
+            if str(grp.name) == 'uilfest': continue
+            print ' '+str(grp.name)
+            assert rel['until'] is None
+            # Es.rcol.update({'_id': rel['_id']},
+            #                {'$set': {'until': datetime.datetime(2015,8,31)}})
+
+
+if __name__ == '__main__':
+    main()
