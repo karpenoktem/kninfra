45b67680c78e09cf09452e6a10d15e73b70869ae
Bram Westerbaan <bram@westerbaan.name>
2017-01-15 18:24:38 +0100

    implements fin_get_account (untested)

--- kn/utils/moniek/fin.py
+++ kn/utils/moniek/fin.py
@@ -1,7 +1,16 @@
 from django.conf import settings
+from koert.gnucash.tools import open_gcf_in_git_repo
+from koert.gnucash.export import get_user_balance
+
 
 
 def fin_get_account(moniek, name, full_name):
-    return {'alles is voor': 'bassie'}
+    gcf = open_gcf_in_git_repo(
+            settings.FIN_REPO_PATH,
+            settings.FIN_FILENAME,
+            cachepath=settings.FIN_CACHE_PATH)
+    return get_user_balance(gcf.books[0],
+            settings.FIN_CREDITORS_ACCOUNT+":"+full_name,
+            settings.FIN_DEBITORS_ACCOUNT+":"+full_name)
 
 # vim: et:sta:bs=2:sw=4:
