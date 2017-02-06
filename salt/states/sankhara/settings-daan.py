7d3907e25ddfdce22cec585153f4fcb9290d1625
Bas Westerbaan <bas@westerbaan.name>
2017-02-06 19:07:30 +0100

    salt: disable wiki in giedo&daan

--- salt/states/sankhara/settings-daan.py
+++ salt/states/sankhara/settings-daan.py
@@ -4,8 +4,8 @@ CHUCK_NORRIS_HIS_SECRET = '{{ pillar['secrets']['chucknorris'] }}'
 SECRET_KEY = '{{ pillar['secrets']['django_secret_key'] }}'
 ALLOWED_API_KEYS = ('{{ pillar['secrets']['apikey'] }}',)
 MAILMAN_DEFAULT_PASSWORD = '{{ pillar['secrets']['mailman_default'] }}'
-WIKI_MYSQL_SECRET = ('localhost', 'wiki',
-                     '{{ pillar['secrets']['mysql_wiki'] }}', 'wiki')
+# WIKI_MYSQL_SECRET = ('localhost', 'wiki',
+#                      '{{ pillar['secrets']['mysql_wiki'] }}', 'wiki')
 FORUM_MYSQL_SECRET = ('localhost', 'forum',
                       '{{ pillar['secrets']['mysql_forum'] }}', 'forum')
 WOLK_MYSQL_SECRET = ('localhost', 'wolk',
