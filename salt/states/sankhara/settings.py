74ae1cfc1b3fd0db80eec525220557ce648846ec
Bas Westerbaan <bas@westerbaan.name>
2017-02-11 10:59:36 +0100

    Install mediawiki from backports. Fixes #421

--- salt/states/sankhara/settings.py
+++ salt/states/sankhara/settings.py
@@ -4,8 +4,8 @@ CHUCK_NORRIS_HIS_SECRET = '{{ pillar['secrets']['chucknorris'] }}'
 SECRET_KEY = '{{ pillar['secrets']['django_secret_key'] }}'
 ALLOWED_API_KEYS = ('{{ pillar['secrets']['apikey'] }}',)
 MAILMAN_DEFAULT_PASSWORD = '{{ pillar['secrets']['mailman_default'] }}'
-# WIKI_MYSQL_SECRET = ('localhost', 'giedo',
-#                     '{{ pillar['secrets']['mysql_giedo'] }}', 'wiki')
+WIKI_MYSQL_SECRET = ('localhost', 'giedo',
+                     '{{ pillar['secrets']['mysql_giedo'] }}', 'wiki')
 # FORUM_MYSQL_SECRET = ('localhost', 'giedo',
 #                        '{{ pillar['secrets']['mysql_giedo'] }}', 'forum')
 # WOLK_MYSQL_SECRET = ('localhost', 'giedo',
