c85df7f7e4f6ab2f24d83b6abab7a1c2c75e586d
Bas Westerbaan <bas@westerbaan.name>
2017-06-21 16:08:13 +0200

    django 1.10: urls: use views directly

--- kn/urls.py
+++ kn/urls.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.generic import RedirectView
 
 from kn.base.backports import i18n_patterns
+from kn.leden import views
 
 urlpatterns = [
     url(r'^favicon.ico$', RedirectView.as_view(
@@ -16,7 +17,7 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     url(_(r'^groups/(?P<subdir>[^/]+)/(?P<path>.*)'),
-        'kn.browser.views.homedir', {'root': '/mnt/phassa/groups/'},
+        views.homedir, {'root': '/mnt/phassa/groups/'},
         name='groupfolder'),
     url(_(r'^smoelen/'), include('kn.leden.urls')),
     url(_(r'^activiteit/'), include('kn.subscriptions.urls')),
@@ -24,8 +25,8 @@ urlpatterns += i18n_patterns(
     url(_(r'^poll/'), include('kn.poll.urls')),
     url(_(r'^accounts/login/$'), auth.views.login, name='login'),
     url(_(r'^accounts/logout/$'), auth.views.logout_then_login, name='logout'),
-    url(_(r'^accounts/rauth/$'), 'kn.leden.views.rauth', name='rauth'),
-    url(_(r'^accounts/api/$'), 'kn.leden.views.accounts_api', name='auth-api'),
+    url(_(r'^accounts/rauth/$'), views.rauth, name='rauth'),
+    url(_(r'^accounts/api/$'), views.accounts_api, name='auth-api'),
     url(_(r'^moderatie/'), include('kn.moderation.urls')),
     url(_(r'^planning/'), include('kn.planning.urls')),
     url(r'', include('kn.base.urls')),
