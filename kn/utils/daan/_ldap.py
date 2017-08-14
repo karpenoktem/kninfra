karpenoktem/kninfra

commit dca51070167dcb680dde11b1b2514034a18e6c6d

Various Python 3 compatibility fixes

kn/utils/daan/_ldap.py
@@ -3,6 +3,7 @@ import logging
 
 import ldap
 import ldap.modlist
+import six
 
 from django.conf import settings
 
@@ -55,15 +56,15 @@ def apply_ldap_changes(daan, changes):
     l.bind_s(settings.LDAP_USER, settings.LDAP_PASS)
     try:
         for uid in changes['remove']:
-            dn = 'uid=%s,%s' % (uid, settings.LDAP_BASE)
+            dn = 'uid=' + uid + ',' + settings.LDAP_BASE
             l.delete_s(dn)
         for uid, mail, sn, cn in changes['upsert']:
-            dn = 'uid=%s,%s' % (uid, settings.LDAP_BASE)
+            dn = 'uid=' + uid + ',' + settings.LDAP_BASE
             res = l.search_s(settings.LDAP_BASE, ldap.SCOPE_ONELEVEL,
-                             'uid=%s' % uid)
+                             'uid=' + uid)
             if not res:
-                entry = {'objectClass': ['inetOrgPerson'],
-                         'uid': [uid],
+                entry = {'objectClass': [b'inetOrgPerson'],
+                         'uid': [uid.encode()],
                          'sn': [sn],
                          'cn': [cn],
                          'mail': [mail]}
@@ -74,7 +75,7 @@ def apply_ldap_changes(daan, changes):
                    'sn': _o['sn'][0],
                    'cn': _o['cn'][0],
                    'mail': _o['mail'][0]}
-            new = {'uid': [uid],
+            new = {'uid': [uid].encode(),
                    'sn': [sn],
                    'cn': [cn],
                    'mail': [mail]}

kn/utils/giedo/_ldap.py
@@ -26,11 +26,11 @@ def generate_ldap_changes(giedo):
                                           entry['sn'][0],
                                           entry['cn'][0])
         for u in users:
-            uid = str(u.name)
+            uid = str(u.name).encode()
             should_have = (uid,
-                           u.canonical_email,
-                           u.last_name.encode('utf-8'),
-                           six.text_type(u.humanName).encode('utf-8'))
+                           u.canonical_email.encode(),
+                           u.last_name.encode(),
+                           six.text_type(u.humanName).encode())
             if uid in unaccounted_for:
                 unaccounted_for.remove(uid)
                 if in_ldap[uid] == should_have:

kn/utils/daan/forum.py
@@ -20,7 +20,7 @@ def forum_setpass(daan, user, password):
         user=creds[1],
         password=creds[2],
         db=creds[3],
-        charset='utf-8'
+        charset='utf8'
     )
     try:
         with dc.cursor() as c:
@@ -46,7 +46,7 @@ def apply_forum_changes(daan, changes):
         user=creds[1],
         password=creds[2],
         db=creds[3],
-        charset='utf-8'
+        charset='utf8'
     )
     try:
         with dc.cursor() as c:

kn/utils/daan/wiki.py
@@ -21,7 +21,7 @@ def apply_wiki_changes(daan, changes):
         user=creds[1],
         password=creds[2],
         db=creds[3],
-        charset='utf-8'
+        charset='utf8'
     )
     try:
         for user, realname, email in changes['add']:

kn/utils/giedo/forum.py
@@ -20,7 +20,7 @@ def generate_forum_changes(self):
         user=creds[1],
         password=creds[2],
         db=creds[3],
-        charset='utf-8'
+        charset='utf8'
     )
     try:
         with dc.cursor() as c:

kn/utils/giedo/mailman.py
@@ -27,16 +27,17 @@ def generate_mailman_changes(giedo):
     # Find the current members of the mailing lists and find which
     # mailing lists are missing.
     for g in mm_groups:
-        gid2name[g._id] = str(g.name)
-        if str(g.name) in ml_membership:
-            membership_to_check[str(g.name)] = set(ml_membership[str(g.name)])
+        name = str(g.name).encode()
+        gid2name[g._id] = name
+        if name in ml_membership:
+            membership_to_check[name] = set(ml_membership[name])
         else:
-            todo['create'].append((str(g.name), six.text_type(g.humanName)))
-            membership_to_check[str(g.name)] = set()
+            todo['create'].append((name, six.text_type(g.humanName)))
+            membership_to_check[name] = set()
 
     # Check which memberships are missing in the current mailing lists
     for rel in mm_rels:
-        em = rel['who'].canonical_email
+        em = rel['who'].canonical_email.encode()
         gname = gid2name[rel['with']]
         if em not in membership_to_check[gname]:
             if gname not in todo['add']:

kn/utils/giedo/postfix.py
@@ -39,7 +39,7 @@ def generate_postfix_map(giedo):
         if id_email not in tbl:
             tbl[id_email] = (e.canonical_email,)
     # handle the non-mailman groups
-    for rel in Es.query_relations(_with=non_mailman_groups.keys(),
+    for rel in Es.query_relations(_with=list(non_mailman_groups),
                                   _from=dt_now, until=dt_now, how=None):
         e = non_mailman_groups[rel['with']]
         email = id2email.get(rel['who'])

kn/utils/giedo/quassel.py
@@ -15,7 +15,7 @@ def generate_quassel_changes(giedo):
         settings.QUASSEL_CONFIGDIR,
         'quassel-storage.sqlite')
     if not os.path.exists(db_path):
-        os.logging.warn('quassel: %s does not exist. Skipping.', db_path)
+        logging.warn('quassel: %s does not exist. Skipping.', db_path)
         return None
 
     # Check which users are currently on the core

commit bb819b745c8bbcec822fd0b96f50da7ff5907e96

Fix typo in giedo ldap sync
Introduced in dca51070167dcb680dde11b1b2514034a18e6c6d (Python 3
migration)

kn/utils/daan/_ldap.py
@@ -81,7 +81,7 @@ def apply_ldap_changes(daan, changes):
                    'sn': _o['sn'][0],
                    'cn': _o['cn'][0],
                    'mail': _o['mail'][0]}
-            new = {'uid': [uid].encode(),
+            new = {'uid': [uid.encode()],
                    'sn': [sn],
                    'cn': [cn],
                    'mail': [mail]}