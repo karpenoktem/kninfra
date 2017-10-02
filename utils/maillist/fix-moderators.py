cf013287b4966dee00c9476b2fe54bbca45c86e6
Ayke van Laethem <ayke@karpenoktem.nl>
2017-10-02 15:56:07 +0200

    Set secretaris@ as moderator for lists that don't have one yet

---
+++ utils/maillist/fix-moderators.py
@@ -0,0 +1,45 @@
+# vim: et:sta:bs=2:sw=4:
+import argparse
+
+import _import  # noqa: F401
+
+from kn.utils.hans.mailman import MailList, Utils
+
+SECRETARY = 'secretaris@karpenoktem.nl'
+ALLOWED_OWNERS = ['wortel@karpenoktem.nl',
+                  SECRETARY]
+
+
+def main(dryrun=True):
+    '''
+    Set the secretary as moderator for every maillist that holds incoming mail
+    and doesn't have a moderator yet. Otherwise only root gets a notification
+    and mail might be unintentionally not sent.
+    '''
+    for name in sorted(Utils.list_names()):
+        ml = MailList.MailList(name, True)
+        try:
+            changed = False
+            if ml.generic_nonmember_action == 0: # Accept
+                pass
+            elif ml.generic_nonmember_action == 1: # Hold
+                if ml.moderator == []:
+                    changed = True
+                    ml.moderator = [SECRETARY]
+                    print('%-20s moderator set to %s' % (name, SECRETARY))
+                elif ml.moderator != [SECRETARY]:
+                    print('%s: warning: moderator is %s' % (name, ml.moderator))
+            else:
+                print('%s: warning: generic_nonmember_action=%d' % (name, ml.generic_nonmember_action))
+            if changed and not dryrun:
+                ml.Save()
+        finally:
+            ml.Unlock()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--apply', action='store_true')
+    options = parser.parse_args()
+    dryrun = not options.apply
+    main(dryrun)
