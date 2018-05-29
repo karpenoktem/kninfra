741f2828565424d7e09c07ec45d78fdcfe211300
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-05-29 22:16:41 +0200

    utils/leden/check-email: Update to common email code

--- utils/leden/check-email.py
+++ utils/leden/check-email.py
@@ -5,11 +5,8 @@ import sys
 
 from common import args_to_users
 
-from django.core.mail import send_mail
-from django.template import Context, Template
-from django.utils.six.moves import cStringIO
-
 import kn.leden.entities as Es
+from kn.base.mail import render_then_email
 from kn.leden.mongo import _id
 
 DAYS_IN_YEAR = 365.242199
@@ -18,14 +15,6 @@ DAYS_IN_YEAR = 365.242199
 def check_email():
     comm_ids = [_id(x) for x in Es.by_name('comms').get_bearers()]
     list_ids = [_id(x) for x in Es.by_name('lists-opted').get_bearers()]
-    with open('check-email.template') as f:
-        template_text = cStringIO()
-        for line in f:
-            if line.endswith("\\\n"):
-                template_text.write(line[:-2])
-            else:
-                template_text.write(line)
-        templ = Template(template_text.getvalue())
     for m in args_to_users(sys.argv[1:]):
         rels = m.get_related()
         rels = sorted(rels, key=lambda x: Es.entity_humanName(x['with']))
@@ -42,14 +31,13 @@ def check_email():
             else:
                 others.append(rel)
         print(m.name)
-        em = templ.render(Context({
-            'u': m,
-            'comms': comms,
-            'lists': lists,
-            'others': others}))
-        send_mail('Controle Karpe Noktem ledenadministratie',
-                  em, 'secretaris@karpenoktem.nl',
-                  [m.primary_email])
+        render_then_email('leden/check-email.mail.txt',
+                          m.primary_email,
+                          {'u': m,
+                           'comms': comms,
+                           'lists': lists,
+                           'others': others},
+                          from_email='secretaris@karpenoktem.nl')
 
 
 if __name__ == '__main__':
