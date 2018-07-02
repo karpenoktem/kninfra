b22288fb6d1b499544ede2e3d9fad2e5cdba225a
Ayke van Laethem <aykevanlaethem@gmail.com>
2018-07-02 19:59:03 +0200

    base: Generate default passwords from a cryptographic source

--- kn/base/_random.py
+++ kn/base/_random.py
@@ -1,4 +1,5 @@
-import random
+import os
+import struct
 
 from django.utils.six.moves import range
 
@@ -7,10 +8,15 @@ NUM = '1234567890'
 ALPHANUMUL = ALPHA + ALPHA.upper() + NUM
 
 
-def pseudo_randstr(length=12, cs=ALPHANUMUL):
+def pseudo_randint():
+    # generate 4 bytes and decode it as a 32-bit unsigned number
+    return struct.unpack('I', os.urandom(4))[0]
+
+
+def pseudo_randstr(length=12, chars=ALPHANUMUL):
     ret = ''
     for i in range(length):
-        ret += cs[random.randint(0, len(cs) - 1)]
+        ret += chars[pseudo_randint() % len(chars)]
     return ret
 
 # vim: et:sta:bs=2:sw=4:
