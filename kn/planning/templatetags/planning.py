090640ff8b51c775021485fcd948af4838c57055
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-04-06 01:18:36 +0200

    planning: only make planning pool clickable if it is editable

--- kn/planning/templatetags/planning.py
+++ kn/planning/templatetags/planning.py
@@ -7,4 +7,9 @@ register = template.Library()
 def last_shift_filter(worker, pool):
     return pool.last_shift(worker)
 
+
+@register.filter(name='may_manage')
+def may_manage_filter(pool, user):
+    return pool.may_manage(user)
+
 # vim: et:sta:bs=2:sw=4:
