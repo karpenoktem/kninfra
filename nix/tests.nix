87d5c6f11b4256640002da93e6ac1fd17a7dc4eb
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-08-25 16:30:08 +0200

    vm tests: test saslauthd

--- nix/tests.nix
+++ nix/tests.nix
@@ -1,4 +1,4 @@
-{system, nixosTest, inputs, curl, kn}:
+{system, nixosTest, inputs, curl, kn, cyrus_sasl}:
 let
   infra = import ./infra.nix;
 in
@@ -12,6 +12,8 @@ nixosTest ({
   testScript = ''
     machine.wait_for_unit("kndjango.socket")
     machine.wait_for_unit("nginx.service")
+    machine.wait_for_unit("saslauthd.service")
+    machine.succeed("${cyrus_sasl}/bin/testsaslauthd -u admin -p 'CHANGE ME'")
     machine.succeed("${curl}/bin/curl -f -LI http://localhost/")
     machine.succeed("${kn.puppetCheck}/bin/kn-puppet http://localhost admin 'CHANGE ME'")
     machine.succeed("${kn.puppetCheck}/bin/kn-puppet http://localhost test 'CHANGE ME'")
